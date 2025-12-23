976e80077d4c4dfebd09543091ae74dce83db3bc
이백행 <leebh@itcen.com>
2025-12-23 15:51:17 +0900

    swat-proc-jni

---
+++ swat-proc-jni/pc/t2.h
@@ -0,0 +1,13 @@
+/*
+ * t2.h
+ *
+ *  Created on: 2025. 12. 23.
+ *      Author: leebh
+ */
+
+#ifndef T2_H_
+#define T2_H_
+
+int t_select(void);
+
+#endif /* T2_H_ */
---
+++ swat-proc-jni/src/swat-proc-jni.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : swat-proc-jni.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "t2.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	t_select();
+
+	return EXIT_SUCCESS;
+}
